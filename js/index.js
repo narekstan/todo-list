fc7f3cf5d80188c69e0c45d554af4bc1e95a60c0
narektovmas <narektovmasyan1705@gmail.com>
2016-07-25 01:50:24 -0700

    Making a TODO list with add/remove/clear ops!

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+var add = document.getElementById("add");
+var rem = document.getElementById("remove");
+var clear = document.getElementById("clear");
+
+var item = document.getElementById("get-todo");
+
+
+
+add.addEventListener("click", function() {
+	// body...
+	var parag = document.createElement("P");
+
+	var itemVal = document.createTextNode(item.value);
+	parag.appendChild(itemVal);
+
+	document.querySelector(".input").appendChild(parag);
+})
+
+
+rem.addEventListener("click", function(argument) {
+	// body...
+	var lst = document.querySelector(".input");
+	lst.removeChild(lst.childNodes[0]);
+})
+
+
+clear.addEventListener("click", function(argument) {
+	// body...
+	document.querySelector(".input").innerHTML = "";
+})
